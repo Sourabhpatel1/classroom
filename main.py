6f7eba6c3548054c18a783cd77feb57ab65b992f
sourabh <sourabh.advo@gmail.com>
2022-10-01 20:14:02 +0530

    4th commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def create_app():
     app.include_router(base_router)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins = ["http://localhost:5173"],
+        allow_origins = ["https://calssroom-front-production.up.railway.app"],
         allow_credentials=True,
         allow_methods=["GET","POST","PUT","DELETE"],
         allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie"]
@@ -25,4 +25,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host='0.0.0.0', port=os.getenv("PORT", default=5000), log_level="info")
