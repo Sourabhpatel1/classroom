3d01c516bb95c4251a076add8a40558192a1d0fe
sourabh <sourabh.advo@gmail.com>
2022-10-01 19:38:30 +0530

    3 commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routes import base_router
-from core.settings import settings
+import uvicorn
+import os
 
 def create_database():
     Base.metadata.create_all(bind=engine)
@@ -23,3 +24,5 @@ def create_app():
 app = create_app()
 
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
